394d755434691acb07141c9316258d8db7acf56d
Horațiu Udrea <udrea.horatiu@gmail.com>
2019-04-19 10:07:55 +0300

    Added time complexities

--- SecondPriorityQueue.h
+++ SecondPriorityQueue.h
@@ -31,17 +31,21 @@ public:
     explicit SecondPriorityQueue(Relation r);
 
     //adds an element with a priority to the queue
+    //Complexity: O(log(n))
     void push(TElem e, TPriority p);
 
     //returns the element with the second highest priority with respect to the relation
     //throws exception if the queue has less than 2 elements
+    //Complexity: Theta(1)
     Element top() const;
 
     //removes and returns the element with the second highest priority
     //throws exception if the queue has less than 2 elements
+    //Complexity: O(log(n))
     Element pop();
 
     //checks if the queue has at most one element
+    //Complexity: Theta(1)
     bool atMostOne() const;
 
     //destructor
